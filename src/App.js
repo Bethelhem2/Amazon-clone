420a17b1ce09462df6428720c886388c0006a3c5
Bethelhem2 <bettywoldeamanuel@gmail.com>
2024-02-28 17:12:55 +0300

    context API example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Routing from './Router';
+// import ComponentA from './ComponentA';
+// import ComponentB from './ComponentB';
+// import { ThemeProvider } from './contextProvider.jsx';
 
 
 
 
 function App() {
   return (
+    // <ThemeProvider>
+    //   <ComponentA/>
+    //   <ComponentB/>
+    // </ThemeProvider>
    <Routing/>
+
   );
 }
 
