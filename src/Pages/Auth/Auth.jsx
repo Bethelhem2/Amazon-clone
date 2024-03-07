361881723964bff1db0152d4bd19849f3e341e1b
Bethelhem2 <bettywoldeamanuel@gmail.com>
2024-03-08 00:52:48 +0300

    protected route

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import classes from "./Auth.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { auth } from "../../Utility/firebase";
 import {
   signInWithEmailAndPassword,
@@ -21,6 +21,8 @@ function Auth() {
 
   const [{ user }, dispatch] = useContext(DataContext);
   const navigate = useNavigate();
+  const navStateData = useLocation();
+  console.log(navStateData)
   const authHandler = async (e) => {
     e.preventDefault();
     // console.log(e.target.name);
@@ -34,7 +36,7 @@ function Auth() {
             user: userInfo.user,
           });
           setLoading({ ...loading, signIn: false });
-          navigate("/");
+          navigate(navStateData?.state?.redirect || '/');
         })
         .catch((err) => {
           setError(err.message);
@@ -49,7 +51,7 @@ function Auth() {
             user: userInfo.user,
           });
           setLoading({ ...loading, signup: false });
-          navigate("/");
+          navigate(navStateData?.state?.redirect || "/");
         })
         .catch((err) => {
           setError(err.message);
@@ -70,6 +72,19 @@ function Auth() {
       {/* {form} */}
       <div className={classes.login__container}>
         <h1>Sign In</h1>
+        {
+          navStateData?.state?.msg && (
+            <small style={{
+              padding: '5px',
+              textAlign: 'center',
+              color: 'red',
+              fontWeight: 'bold',
+            }}
+            >
+              {navStateData?.state?.msg}
+            </small>
+          )
+        }
         <form action="">
           <div>
             <label htmlFor="email">Email</label>
