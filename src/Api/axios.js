2c9deae2b3516a33db430735b7e080a8815fd2ea
Bethelhem2 <bettywoldeamanue@gmail.com>
2024-07-01 07:57:14 +0530

    backend baseurl

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-8723c/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-8723c/us-central1/api",
   // deployed version of amazon server on render.com
+  baseURL: 'https://amazone-api-deploy-2lwp.onrender.com'
 
-  // https://amazone-api-deploy-2lwp.onrender.com
 });
 
 export { axiosInstance };
